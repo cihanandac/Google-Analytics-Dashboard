27341420ac1cc9d0eb67251be8e8ae5e2cb4ee9f
cihanandac <cihanandac@gmail.com>
2022-08-30 01:46:52 +0300

    build:  google authentication system

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,51 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { renderButton, checkSignedIn } from "./lib/out";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import ChartsMonthly from "./components/ChartsMonthly";
 import ChartsDaily from "./components/ChartsDaily";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 
 function App() {
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [data, setData] = useState([]);
 
-  const updateSignin = (signedIn) => {
-    //(3)
-    setIsSignedIn(signedIn);
-    if (!signedIn) {
-      renderButton();
-    }
-  };
-
-  const init = () => {
-    //(2)
-    checkSignedIn()
-      .then((signedIn) => {
-        updateSignin(signedIn);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  useEffect(() => {
-    window.gapi.load("auth2", init); //(1)
-  }, []);
-
   return (
-    <Router>
-      <div className="App">
-        <Navbar isSignedIn={isSignedIn} />
-        {!isSignedIn ? (
-          <div>
-            <div id="signin-button"></div>
-          </div>
-        ) : (
-          <div className="content">
-            <Routes>
-              <Route
-                exact
-                path="/ChartsMonthly"
-                element={<ChartsMonthly data={data} setData={setData} />}
+    <GoogleOAuthProvider clientId="67149944135-4608iad6up8cleubnedprrq7kncq5hsg.apps.googleusercontent.com">
+      <Router>
+        <div className="App">
+          <Navbar isSignedIn={isSignedIn} />
+          {!isSignedIn ? (
+            <div>
+              <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                  console.log(credentialResponse);
+                  setIsSignedIn(true);
+                }}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
               />
-              <Route
-                exact
-                path="/ChartsDaily"
-                element={<ChartsDaily data={data} setData={setData} />}
-              />
-            </Routes>
-          </div>
-        )}
-      </div>
-    </Router>
+            </div>
+          ) : (
+            <div className="content">
+              <Routes>
+                <Route
+                  exact
+                  path="/ChartsMonthly"
+                  element={<ChartsMonthly data={data} setData={setData} />}
+                />
+                <Route
+                  exact
+                  path="/ChartsDaily"
+                  element={<ChartsDaily data={data} setData={setData} />}
+                />
+              </Routes>
+            </div>
+          )}
+        </div>
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
